127af9b27775f4a988d4d69835aa8417ec841e55
stzups <griffinht@gmail.com>
2021-12-12 16:43:01 -0500

    fix typo

--- hot-desktop/setup.sh
+++ hot-desktop/setup.sh
@@ -3,7 +3,7 @@
 /hot-desktop/setup/system.sh
 /hot-desktop/setup/users.sh
 /hot-desktop/setup/openssh-server.sh
-/hot-desktop/setup/static-ip.sh
+/hot-desktop/setup/network.sh
 
 # cleanup
 rm -r /hot-desktop

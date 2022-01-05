c963f7b201fc47db764effe2c17a2071c217bc9c
stzups <griffinht@gmail.com>
2022-01-04 22:22:14 -0500

    fix envy-laptop.sh

--- debian/envy-laptop/envy-laptop.sh
+++ debian/envy-laptop/envy-laptop.sh
@@ -2,7 +2,7 @@
 
 ./build.sh 192.168.0.254:8080 192.168.0.6
 
-cat << EOF > html/preseed
+cat << 'EOF' >> html/preseed
 d-i preseed/early_command string \
     /bin/sh -c "modprobe atl1c && echo "1969 1083" > /sys/bus/pci/drivers/atl1c/new_id"; \
     in-target /bin/sh -c "modprobe atl1c && echo "1969 1083" > /sys/bus/pci/drivers/atl1c/new_id";

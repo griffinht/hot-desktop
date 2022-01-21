36be8674a008fea958ef667cf06b3e52d91755cf
stzups <griffinht@gmail.com>
2022-01-21 16:05:44 -0500

    make host and external address configurable

--- mikrotik/mikrotik.sh
+++ mikrotik/mikrotik.sh
@@ -1,15 +1,20 @@
-ssh user@192.168.0.1
+#ssh user@192.168.0.1
 
+HOST=192.168.0.6
+EXTERNAL_ADDRESS=98.21.46.184
+
+cat << EOF
 # http with nat hairpin
 #https://help.mikrotik.com/docs/display/ROS/NAT#NAT-HairpinNAT
 #https://forum.mikrotik.com/viewtopic.php?t=172380
-ip firewall nat add action=masquerade chain=srcnat dst-address=192.168.0.5 out-interface-list=LAN protocol=tcp src-address=192.168.0.0/24
+ip firewall nat add action=masquerade chain=srcnat dst-address=$HOST out-interface-list=LAN protocol=tcp src-address=192.168.0.0/24
 # nginx
-ip firewall nat add chain=dstnat action=dst-nat dst-address=98.21.46.184 dst-port=80 to-addresses=192.168.0.5 protocol=tcp
-ip firewall nat add chain=dstnat action=dst-nat dst-address=98.21.46.184 dst-port=443 to-addresses=192.168.0.5 protocol=tcp
+ip firewall nat add chain=dstnat action=dst-nat dst-address=$EXTERNAL_ADDRESS dst-port=80 to-addresses=$HOST protocol=tcp
+ip firewall nat add chain=dstnat action=dst-nat dst-address=$EXTERNAL_ADDRESS dst-port=443 to-addresses=$HOST protocol=tcp
 
 # wireguard (on port 123 to circumvent port blocking)
-ip firewall nat add chain=dstnat action=dst-nat dst-address=98.21.46.184 dst-port=3108 to-addresses=192.168.0.5 protocol=udp
+ip firewall nat add chain=dstnat action=dst-nat dst-address=$EXTERNAL_ADDRESS dst-port=3108 to-addresses=$HOST protocol=udp
 
 # minecraft
-ip firewall nat add chain=dstnat action=dst-nat dst-address=98.21.46.184 dst-port=25565 to-addresses=192.168.0.5 protocol=tcp
+ip firewall nat add chain=dstnat action=dst-nat dst-address=$EXTERNAL_ADDRESS dst-port=25565 to-addresses=$HOST protocol=tcp
+EOF
